9ffab1cd109d724d6940d2061ae810c720aa1d38
local_virtual_ubutnu <qwe1515882358@gmail.com>
2024-03-11 21:33:26 +0800

    3 error commit

--- hello_fuc.py
+++ hello_fuc.py
@@ -1,6 +1,8 @@
 def hello(mesg="hello kitty"):
     if type(mesg) != str:
         raise ValueError("type error: not string")
+    # eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    # eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
     print(mesg)
 
 if __name__ == '__main__':
