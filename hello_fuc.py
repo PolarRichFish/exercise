9b67d2965451d800cc7c2321433ce58d2b66445d
local_virtual_ubutnu <qwe1515882358@gmail.com>
2024-03-12 14:37:21 +0800

    dev del error

--- hello_fuc.py
+++ hello_fuc.py
@@ -9,11 +9,6 @@ def over_p():
 def hello(mesg="hello kitty"):
     if type(mesg) != str:
         raise ValueError("type error: not string")
-    # eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-    # eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-    # eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-    # eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-    # eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
     print(mesg)
     over_p()
 
