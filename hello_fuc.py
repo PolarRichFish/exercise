b49bc50c1b5fb6fe6a94cd64f6c0462398f2f396
localwin <qwe1515882358@gmail.com>
2024-03-12 11:12:03 +0800

    win增加 build message

--- hello_fuc.py
+++ hello_fuc.py
@@ -1,7 +1,12 @@
+
+def build_message(user="kitty"):
+    message = "hello " + user
+    return message
+
 def hello(mesg="hello kitty"):
     if type(mesg) != str:
         raise ValueError("type error: not string")
     print(mesg)
 
 if __name__ == '__main__':
-    hello()
+    hello(build_message())
