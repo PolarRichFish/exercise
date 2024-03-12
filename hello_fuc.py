938424d082508a67ea8b9e72bb3ac0561ba8db01
local_virtual_ubutnu <qwe1515882358@gmail.com>
2024-03-12 14:28:24 +0800

    hellodev branch

--- hello_fuc.py
+++ hello_fuc.py
@@ -3,6 +3,9 @@ def build_message(user="kitty"):
     message = "HELLO " + user
     return message
 
+def over_p():
+    print("it is done")
+
 def hello(mesg="hello kitty"):
     if type(mesg) != str:
         raise ValueError("type error: not string")
@@ -12,6 +15,7 @@ def hello(mesg="hello kitty"):
     # eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
     # eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
     print(mesg)
+    over_p()
 
 if __name__ == '__main__':
     hello(build_message())
